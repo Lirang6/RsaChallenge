f00475e4004d4a0664a9e8ea3645ffbf44626f23
nadirboni <nadirboniy6@walla.co.il>
2020-12-05 17:09:15 +0200

    better random

--- prime est v3.5/main.py
+++ prime est v3.5/main.py
@@ -6,13 +6,15 @@ import functions
 #### enter search data here: ####
 number = 2211282552952966643528108525502623092761208950247001539441374831912882294140200198651272972656974659908590033003140005117074220456085927635795375718595429883895870922923849100670303412462054578456641366454068421436129301769402084639106587591479425143514458199
 how_many_to_search = 10000000
-start = 10000000000000000000000000000000000000000000000000000000000000
+start = 10000000000000000000000000000000000000000000000000000000000001 ### HAS TO BE ODD!!!
 end = round((number ** 0.5) + 0.5)
 
 #### search function, dont change ####
 t1 = time.perf_counter()
+if start % 2 != 0:
+    print("start number has to be odd!")
 for i in range(how_many_to_search+1):
-    ran_num = random.randrange(start, end+1)
+    ran_num = random.randrange(start, end+1, 2)
     if functions.find_init_est(ran_num) != 0:
         div_num = number/ran_num
         if div_num == int(div_num):
